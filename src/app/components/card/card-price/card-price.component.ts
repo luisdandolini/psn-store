875e3963a5da359ef3448679c38dc7b1b875ec18
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-11-25 17:31:22 -0300

    feat: create components card-label and card price

---
+++ src/app/components/card/card-price/card-price.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-price',
+  templateUrl: './card-price.component.html',
+  styleUrls: ['./card-price.component.scss']
+})
+export class CardPriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
