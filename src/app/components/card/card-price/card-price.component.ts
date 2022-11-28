621d75221d0b47ff98e0263884a864cd1ec1a8fe
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-11-27 21:07:04 -0300

    feat: add style card price

--- src/app/components/card/card-price/card-price.component.ts
+++ src/app/components/card/card-price/card-price.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-price.component.scss']
 })
 export class CardPriceComponent implements OnInit {
+
+  @Input() gameType: string = 'Digital PS4';
+  @Input() gamePrice: string = 'R$ 399,90'
 
   constructor() { }
 
